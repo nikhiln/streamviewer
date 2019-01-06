b18831457395abf7e65afa9f3c80b54f93acd4f4
nikhil.navadiya <nikhil@awaaz.de>
2019-01-06 19:14:32 +0530

    added few dummy urls

--- server/config/urls.py
+++ server/config/urls.py
@@ -3,12 +3,15 @@ from django.urls import include, path
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.views import defaults as default_views
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('home/', TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    path('about/', TemplateView.as_view(template_name="pages/about.html"), name="about"),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
-    # path("accounts/", include("allauth.urls")),
+    path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
     path(
         "api/",
