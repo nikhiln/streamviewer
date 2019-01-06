f2bb24b5bfd2d9f2df7d7e3ff78f7757d3e23ecc
nikhil.navadiya <nikhil@awaaz.de>
2019-01-06 21:17:44 +0530

    handled logout for consecutive login case

--- server/streamviewer/api/urls.py
+++ server/streamviewer/api/urls.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.urls import path
 from rest_framework.routers import DefaultRouter
+from rest_auth.views import LogoutView
 
 from streamviewer.users.api import GoogleLoginAPIView
 from streamviewer.main.api import YoutubeLiveStreamAPIView, LiveStreamStatAPIView, LiveStreamChatHistoryAPIView
@@ -14,7 +15,8 @@ router.register(r'youtube/stream-chat', LiveStreamChatHistoryAPIView)
 urlpatterns = [
     path('youtube/stats/', view=LiveStreamStatAPIView.as_view(), name="youtube-live-streams-stats"),
     path('youtube/livestream/', view=YoutubeLiveStreamAPIView.as_view(), name="youtube-live-streams"),
-    path('rest-auth/google/', view=GoogleLoginAPIView.as_view(), name='google_login')
+    path('rest-auth/google/', view=GoogleLoginAPIView.as_view(), name='google_login'),
+    path('rest-auth/logout/', LogoutView.as_view(), name='rest_logout'),
 ]
 
 urlpatterns += router.urls
